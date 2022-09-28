dde5b76ac3b7908a7778b3f597a7466c23d9b382
BasavP <basav@outlook.in>
2022-09-28 17:26:33 +0530

    Implemented Versioning using headers , request params

--- main/java/com/in28minutes/rest/webservices/versioning/VersioniningPersonController.java
+++ main/java/com/in28minutes/rest/webservices/versioning/VersioniningPersonController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioniningPersonController {
 
+
+    //Versioning with URI
+
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson(){
                 return new  PersonV1 ("Bob Charlie");
@@ -14,8 +17,37 @@ public class VersioniningPersonController {
 
 
     @GetMapping("/v2/person")
-    public PersonV2 getFirstVersion2OfPerson(){
+    public PersonV2 getSecondVersionOfPerson(){
         return new  PersonV2 (new Name("Bob","Charlie"));
     }
+
+
+    //versioning with requestParams
+    @GetMapping(path = "/person" ,  params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+        return new  PersonV1 ("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person" ,  params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+        return new  PersonV2 (new Name("Bob","Charlie"));
+    }
+
+
+    //Versioning with headers
+
+
+    @GetMapping(path = "/person/header" ,  headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonHeader(){
+        return new  PersonV1 ("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header" ,  headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonHeader(){
+        return new  PersonV2 (new Name("Bob","Charlie"));
+    }
+
+    //acceptHeader Versioning  to be written here
+
 
 }
