ffb8ad5bf3addd953731e04d5472367557164a6a
BasavP <basav@outlook.in>
2022-09-28 23:00:59 +0530

    added dynamic filtering

--- main/java/com/in28minutes/rest/webservices/filetering/FilteringController.java
+++ main/java/com/in28minutes/rest/webservices/filetering/FilteringController.java
@@ -1,25 +1,55 @@
 package com.in28minutes.rest.webservices.filetering;
 
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 public class FilteringController {
 
     @GetMapping(path = "/filtering")
-    public SomeBean filtering(){
-        return  new SomeBean("value1" , "value2" , "value3");
+    public MappingJacksonValue filtering(){
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
+        //create a filter
+        SimpleBeanPropertyFilter simpleBeanPropertyFilter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("field1","field3");
+
+        //create a filterProvider , adding the above created filter
+        FilterProvider filters = new SimpleFilterProvider()
+                .addFilter("SomeBeanFilter"   //this filtername will be used in the annotation
+                ,simpleBeanPropertyFilter);
+
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
     }
 
     @GetMapping(path = "/filtering-list")
-    public List<SomeBean> filteringList(){
-        return Arrays.asList(new SomeBean("value1" , "value2" , "value3") ,
-                new SomeBean("value4" , "value5" , "value6"));
+    public MappingJacksonValue filteringList(){
+        List<SomeBean> someBeans = Arrays.asList(new SomeBean("value1", "value2", "value3"),
+                new SomeBean("value4", "value5", "value6"));
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBeans);
+        //create a filter
+        SimpleBeanPropertyFilter simpleBeanPropertyFilter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("field1","field3");
+
+        //create a filterProvider , adding the above created filter
+        FilterProvider filters = new SimpleFilterProvider()
+                .addFilter("SomeBeanFilter"   //this filtername will be used in the annotation
+                        ,simpleBeanPropertyFilter);
+
+        mappingJacksonValue.setFilters(filters);
+
+
+        return mappingJacksonValue;
     }
 
 }
--- main/java/com/in28minutes/rest/webservices/filetering/SomeBean.java
+++ main/java/com/in28minutes/rest/webservices/filetering/SomeBean.java
@@ -1,30 +1,32 @@
 package com.in28minutes.rest.webservices.filetering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 
 
-    private String value1;
+    private String field1;
     @JsonIgnore
-    private String value2;
-    private String value3;
+    private String field2;
+    private String field3;
 
     public SomeBean(String value1, String value2, String value3) {
-        this.value1 = value1;
-        this.value2 = value2;
-        this.value3 = value3;
+        this.field1 = value1;
+        this.field2 = value2;
+        this.field3 = value3;
     }
 
-    public String getValue1() {
-        return value1;
+    public String getField1() {
+        return field1;
     }
 
-    public String getValue2() {
-        return value2;
+    public String getField2() {
+        return field2;
     }
 
-    public String getValue3() {
-        return value3;
+    public String getField3() {
+        return field3;
     }
 }
