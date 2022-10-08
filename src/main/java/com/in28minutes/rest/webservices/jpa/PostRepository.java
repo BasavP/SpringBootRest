d2da18292201199762a14c890821779c03feceab
basavp <basav@outlook.in>
2022-10-08 19:43:30 +0530

    Revert "security"
    This reverts commit 54719e0163fd8f808db92e6857633a14eb7b6233.

--- src/main/java/com/in28minutes/rest/webservices/jpa/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.in28minutes.rest.webservices.jpa;
-
-import com.in28minutes.rest.webservices.User.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
--- src/main/java/com/in28minutes/rest/webservices/jpa/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.in28minutes.rest.webservices.jpa;
-
-import com.in28minutes.rest.webservices.User.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
